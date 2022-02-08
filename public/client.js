eea5b61a4019783e3524019d87822de1bd4b164a
Nouman Qureshi <noumanqureshi15@gmail.com>
2022-02-08 18:33:13 +0500

    Add files via upload

--- public/client.js
+++ public/client.js
@@ -3,6 +3,7 @@ const messagecontainer = document.getElementById('message-container');
 const messageForm = document.getElementById('send-button');
 const messageInput = document.getElementById('message-input')
 var mainDiv = document.getElementById("main");
+let count;
 // const canvas = document.getElementById('canvas');
 // const ctx = canvas.getContext('2d');
     // const btn = document.getElementById('btn');
@@ -31,12 +32,17 @@ socket.on('user-disconnect', data =>{
 messageForm.addEventListener('click', e=>{
     e.preventDefault()
     const message = messageInput.value
+    // count = message;
+    // console.log('user_'+ count);
     socket.emit('send-chat-message', message)
     messageInput.value = ''
 })
 
 function appendMessage(message){
-    const gettext = message
+    
+    const gettext = message;
+    count = message;
+    console.log('Counter '+count);
     var g_tag = document.createElementNS("http://www.w3.org/2000/svg","g");
             g_tag.setAttribute("id",gettext+"_user_1");
             document.getElementById("map").appendChild(g_tag);
@@ -48,31 +54,19 @@ function appendMessage(message){
                  createrect.setAttribute("width",12);
                  createrect.setAttribute("height",12);
                  createrect.setAttribute("rx",2);
-                 const animate = document.createElementNS("http://www.w3.org/2000/svg", "animate");
-                 animate.setAttribute("id","rect_animate");
-                 animate.setAttribute("begin","map.click");
-                 const rect_animate_y = document.createElementNS("http://www.w3.org/2000/svg", "animate");
-                 rect_animate_y.setAttribute("id","rect_animate_y");
-                 rect_animate_y.setAttribute("begin","map.click");
-                 //  createrect.setAttribute("style", "fill: rgb(0, 33, 218);");
+                 createrect.style.transition="all 2s";
                  console.log(createrect);
-                 createrect.appendChild(animate);
-                 createrect.appendChild(rect_animate_y);
+            
                  var create_text = document.createElementNS("http://www.w3.org/2000/svg","text");
-                 const text_animate_x = document.createElementNS("http://www.w3.org/2000/svg", "animate");
+              
                  create_text.setAttribute("id",gettext+"text");
                  create_text.setAttribute("class","text");
                  create_text.setAttribute("y",20);
                  create_text.setAttribute("style","fill: rgb(75, 77, 88);");
-                 text_animate_x.setAttribute("id","text_animate_x");
-                 text_animate_x.setAttribute("begin", "map.click");
-             const  text_animate_y = document.createElementNS("http://www.w3.org/2000/svg", "animate");
-                 text_animate_y.setAttribute("id","text_animate_y");
-                 text_animate_y.setAttribute("begin","map.click");
+                 create_text.style.transition = "all 2s";
+           
                  var newtext = document.createTextNode(gettext);
                  create_text.appendChild(newtext);
-                 create_text.appendChild(text_animate_x);
-                 create_text.appendChild(text_animate_y);
                  g_tag.appendChild(createrect);
                  g_tag.appendChild(create_text);
 }
@@ -134,88 +128,63 @@ function appendMessage(message){
 // userInput(playerBall);
 // requestAnimationFrame(mainLoop);
 function transition(valueX, valueY){
-    var  rect_get_id = document.getElementsByClassName("all_rect")[0].id;
-    var text_get_id = document.getElementsByClassName('text')[0].id;
-    var text= document.getElementById(text_get_id);
-    var rect = document.getElementById(rect_get_id);
+    const get_id = count;
+    // var  rect_get_id = document.getElementsByClassName("all_rect")[v].id;
+    // var text_get_id = document.getElementsByClassName('text')[v].id;
+    var text= document.getElementById(get_id+"text");
+    var rect = document.getElementById(get_id);
     var getx= rect.getAttribute("x");
     var gety = rect.getAttribute("y");
-    var rect_animate = document.getElementById("rect_animate");
-    var rect_animate_y = document.getElementById("rect_animate_y");
-    var text_animate_x = document.getElementById("text_animate_x");
-    var text_animate_y = document.getElementById("text_animate_y");
+    // var rect_animate = document.getElementById("rect_animate");
+    // var rect_animate_y = document.getElementById("rect_animate_y");
+    // var text_animate_x = document.getElementById("text_animate_x");
+    // var text_animate_y = document.getElementById("text_animate_y");
 
-                rect_animate.setAttribute("attributeName","x");
-                rect_animate.setAttribute("dur","3s");
-                rect_animate.setAttribute("to",valueX);
-                rect_animate.setAttribute("from", getx);
-                rect_animate.setAttribute("repeatCount", "linear");
+    //             rect_animate.setAttribute("attributeName","x");
+    //             rect_animate.setAttribute("dur","3s");
+    //             rect_animate.setAttribute("to",valueX);
+    //             rect_animate.setAttribute("from", getx);
+    //             rect_animate.setAttribute("repeatCount", "linear");
                 
-                text_animate_x.setAttribute("attributeName","x");
-                text_animate_x.setAttribute("dur","3s");
-                text_animate_x.setAttribute("to",valueX);
-                text_animate_x.setAttribute("from", getx);
-                text_animate_x.setAttribute("repeatCount", "linear");
+    //             text_animate_x.setAttribute("attributeName","x");
+    //             text_animate_x.setAttribute("dur","3s");
+    //             text_animate_x.setAttribute("to",valueX);
+    //             text_animate_x.setAttribute("from", getx);
+    //             text_animate_x.setAttribute("repeatCount", "linear");
 
-                text_animate_y.setAttribute("dur", "3s");
-                text_animate_y.setAttribute("attributeName","y");
-                text_animate_y.setAttribute("to", valueY);
-                text_animate_y.setAttribute("from", gety);
-                text_animate_y.setAttribute("repeatCount", "linear");
+    //             text_animate_y.setAttribute("dur", "3s");
+    //             text_animate_y.setAttribute("attributeName","y");
+    //             text_animate_y.setAttribute("to", valueY);
+    //             text_animate_y.setAttribute("from", gety);
+    //             text_animate_y.setAttribute("repeatCount", "linear");
 
-                rect_animate_y.setAttribute("dur", "3s");
-                rect_animate_y.setAttribute("attributeName","y");
-                rect_animate_y.setAttribute("to", valueY);
-                rect_animate_y.setAttribute("from", gety);
-                rect_animate_y.setAttribute("repeatCount", "linear");
+    //             rect_animate_y.setAttribute("dur", "3s");
+    //             rect_animate_y.setAttribute("attributeName","y");
+    //             rect_animate_y.setAttribute("to", valueY);
+    //             rect_animate_y.setAttribute("from", gety);
+    //             rect_animate_y.setAttribute("repeatCount", "linear");
                 rect.setAttribute("x", valueX);
                 rect.setAttribute("y", valueY);
                 text.setAttribute("x", valueX);
                 text.setAttribute("y", valueY);
 }
 function changeDimensions(click , message) {
-    const get_id = message;
-    var  rect_get_id = document.getElementsByClassName("all_rect")[0].id;
-    var text_get_id = document.getElementsByClassName('text')[0].id;
-    // var rect2 = document.getElementById
-    var text= document.getElementById(text_get_id);
-    var rect = document.getElementById(rect_get_id);
+   
+    const get_id = count;
+    // var  rect_get_id = document.getElementById(v);
+    // var text_get_id = document.getElementById(v);
+    // // var rect2 = document.getElementById
+    var text= document.getElementById(get_id+"text");
+    var rect = document.getElementById(get_id);
     var getx= rect.getAttribute("x");
     var gety = rect.getAttribute("y");
     var x = click.clientX;
     var y = click.clientY;
-    var rect_animate = document.getElementById("rect_animate");
-    var rect_animate_y = document.getElementById("rect_animate_y");
-    var text_animate_x = document.getElementById("text_animate_x");
-    var text_animate_y = document.getElementById("text_animate_y");
-
-                rect_animate.setAttribute("attributeName","x");
-                rect_animate.setAttribute("dur","3s");
-                rect_animate.setAttribute("to",x);
-                rect_animate.setAttribute("from", getx);
-                rect_animate.setAttribute("repeatCount", "linear");
-                
-                text_animate_x.setAttribute("attributeName","x");
-                text_animate_x.setAttribute("dur","3s");
-                text_animate_x.setAttribute("to",x);
-                text_animate_x.setAttribute("from", getx);
-                text_animate_x.setAttribute("repeatCount", "linear");
-
-                text_animate_y.setAttribute("dur", "3s");
-                text_animate_y.setAttribute("attributeName","y");
-                text_animate_y.setAttribute("to", y);
-                text_animate_y.setAttribute("from", gety);
-                text_animate_y.setAttribute("repeatCount", "linear");
-
-                rect_animate_y.setAttribute("dur", "3s");
-                rect_animate_y.setAttribute("attributeName","y");
-                rect_animate_y.setAttribute("to", y);
-                rect_animate_y.setAttribute("from", gety);
-                rect_animate_y.setAttribute("repeatCount", "linear");
-                rect.setAttribute("x", x);
-                rect.setAttribute("y", y);
-                text.setAttribute("x", x)
-                text.setAttribute("y",y);
+    
+    rect.setAttribute("x", x);
+    rect.setAttribute("y", y);
+    text.setAttribute("x", x)
+    text.setAttribute("y",y);
  
     console.log(x+"value x");
     console.log(y+"value y");
