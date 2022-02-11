03a6b15d408a5b99cd0155ede641be6c9ab19a08
Nouman Qureshi <noumanqureshi15@gmail.com>
2022-02-11 13:00:48 +0500

    Add files via upload

--- public/client.js
+++ public/client.js
@@ -7,18 +7,12 @@ var count =[];
 var limit = 5;
 var one;
 socket.emit('newUser', );
-// const canvas = document.getElementById('canvas');
-// const ctx = canvas.getContext('2d');
-    // const btn = document.getElementById('btn');
+
     const ctx = document.getElementById('map');
-    // const btn = document.getElementById('btn');
-    // btn.addEventListener('click',(e)=>{
-    //     const getuser = document.getElementById('input').value;
-    // })
-// let clientavator = {};
 socket.on('chat-message', data =>{
     appendMessage(data)
 })
+// send your position..
 socket.on('updated_x' , value1=>{
     socket.on('updated_y' , value2=>{
         socket.on('update_user',username=>{
@@ -29,7 +23,6 @@ socket.on('updated_x' , value1=>{
                     var gtrans = document.getElementById(get_g_id);
                     var attrvalue = "translate("+value1+","+value2+")";
                     gtrans.setAttribute("transform",attrvalue);
-                    console.log('value is is'+username);
                 }
             }
            
@@ -50,7 +43,7 @@ messageForm.addEventListener('click', e=>{
     socket.emit('send-chat-message', message)
     messageInput.value = ''
 })
-
+//
 function appendMessage(message){
     
     const gettext = message;
@@ -72,7 +65,6 @@ function appendMessage(message){
                  createrect.setAttribute("width",12);
                  createrect.setAttribute("height",12);
                  createrect.setAttribute("rx",2);
-                 console.log(createrect);
             
                  var create_text = document.createElementNS("http://www.w3.org/2000/svg","text");
               
@@ -86,191 +78,11 @@ function appendMessage(message){
                  g_tag.appendChild(createrect);
                  g_tag.appendChild(create_text);
 }
-//setting up the environment
-// putWallsAround(0, 0, canvas.clientWidth, canvas.clientHeight);
-// let startX = 40+Math.random()*560;
-// let startY = 40+Math.random()*400;
-// let playerBall = new Ball(startX, startY, 40, 5);
-// playerBall.player = true;
-// playerBall.maxSpeed = 5;
-// function gameLogic(){}
 
-
-//sending the initial positions to the server
-
-//reacting for new and disconnecting clients
-// btn.addEventListener('click', () =>{
-//     socket.emit('newUser', {x: startX, y: startY});
-//     socket.on('updatePlayers', players => {
-//         const gettext = document.getElementById('input').value;
-//         const g_tag = document.createElementNS("http://www.w3.org/2000/svg","g");
-//         ctx.appendChild(g_tag);
-//         var createrect = document.createElementNS("http://www.w3.org/2000/svg","rect");
-//                      createrect.setAttribute("id",gettext);
-//                      createrect.setAttribute("x",0);
-//                      createrect.setAttribute("y",0);
-//                      createrect.setAttribute("width",12);
-//                      createrect.setAttribute("height",12);
-//                      createrect.setAttribute("rx",2);
-//         var create_text = document.createElementNS("http://www.w3.org/2000/svg","text");
-//         create_text.setAttribute("id","text");
-//         create_text.setAttribute("y",12);
-//         create_text.setAttribute("style","fill: rgb(75, 77, 88);");
-//         var newtext = document.createTextNode(gettext);
-//                      create_text.appendChild(newtext);
-//                      g_tag.appendChild(createrect);
-//                     g_tag.appendChild(create_text);
-//         playersFound = {};
-//         for(let id in players){
-//             if(clientavator[id] === undefined && id !== socket.id){
-//                 clientavator[id] = new Ball (players[id].x, players[id].y, 40, 5);
-//             }
-//             playersFound[id] = true;
-//         }
-//         for(let id in clientavator){
-//             if(!playersFound[id]){
-//                 clientavator[id].remove();
-//                 delete clientavator[id];
-//             }
-//         }
-//     })
-// })
-
-
-// function gameLogic(){
-//     socket.emit('update', {x: playerBall.pos.x, y: playerBall.pos.y});
-// }
-
-// userInput(playerBall);
-// requestAnimationFrame(mainLoop);
 function transition(valueX, valueY){
-        var v1=count[0];
-        var v2=count[1];
-        var v3=count[2];
-        var v4=count[3];
-        var v5=count[4];
-        // if(one === undefined){
-        //     var get_g_id = document.getElementsByClassName('MapUser_MapUser_160Xx')[0].id;
-        //     var gtrans = document.getElementById(get_g_id);
-        //     var attrvalue = "translate("+valueX+","+valueY+")";
-        //     gtrans.setAttribute("transform",attrvalue);
-        //     console.log('value is is'+one);
-        // }
-        
-        // else if(one ==v2){
-
-        //         var get_g_id = document.getElementsByClassName('MapUser_MapUser_160Xx')[0].id;
-        //         var gtrans = document.getElementById(get_g_id);
-        //         var attrvalue = "translate("+valueX+","+valueY+")";
-        //         gtrans.setAttribute("transform",attrvalue);
-        //         console.log('value is is'+one);
-        //         console.log("second wala calling");
-            
-        // }
-        // else if(one ==v3){
-        //     if(one==v1){
-        //         var get_g_id = document.getElementsByClassName('MapUser_MapUser_160Xx')[0].id;
-        //         var gtrans = document.getElementById(get_g_id);
-        //         var attrvalue = "translate("+valueX+","+valueY+")";
-        //         gtrans.setAttribute("transform",attrvalue);
-        //         console.log('value is is'+one);
-        //         console.log("third wala calling");
-        //     }
-        //     if(one==v3){
-        //         var get_g_id = document.getElementsByClassName('MapUser_MapUser_160Xx')[1].id;
-        //         var gtrans = document.getElementById(get_g_id);
-        //         var attrvalue = "translate("+valueX+","+valueY+")";
-        //         gtrans.setAttribute("transform",attrvalue);
-        //         console.log('value is is'+one);
-        //         console.log("third wala calling");
-        //     }
-        //     else{
-        //         var get_g_id = document.getElementsByClassName('MapUser_MapUser_160Xx')[2].id;
-        //         var gtrans = document.getElementById(get_g_id);
-        //         var attrvalue = "translate("+valueX+","+valueY+")";
-        //         gtrans.setAttribute("transform",attrvalue);
-        //         console.log('value is is'+one);
-        //         console.log("third wala else calling");
-        //     }
-            
-        // }
-        // else if(one==v1){
-        //     var get_g_id = document.getElementsByClassName('MapUser_MapUser_160Xx')[2].id;
-        //     var gtrans = document.getElementById(get_g_id);
-        //     var attrvalue = "translate("+valueX+","+valueY+")";
-        //     gtrans.setAttribute("transform",attrvalue);
-        //     console.log('value is is'+one);
-        //     console.log("third wala else calling");
-        // }
-        //     else{
-        //         var get_g_id = document.getElementsByClassName('MapUser_MapUser_160Xx')[1].id;
-        //         var gtrans = document.getElementById(get_g_id);
-        //         var attrvalue = "translate("+valueX+","+valueY+")";
-        //         gtrans.setAttribute("transform",attrvalue);
-        //         console.log('value is is'+one);
-        //         console.log("else wali calling");
-        //     }
-    // const get_id = count;
-    // var  rect_get_id = document.getElementsByClassName("all_rect")[v].id;
-    // var text_get_id = document.getElementsByClassName('text')[v].id;
-    // var text= document.getElementById(get_id+"text");
-    // var rect = document.getElementById(get_id);
-    // var v=0;
-    // if(count[0]){
-    //     var get_g_id = document.getElementsByClassName('MapUser_MapUser_160Xx')[0].id;
-    //         var gtrans = document.getElementById(get_g_id);
-    //         var attrvalue = "translate("+value1+","+value2+")";
-    //         gtrans.setAttribute("transform",attrvalue);
-    //         ++v;
-    // }
-    // if(count[1]){
-    //     var get_g_id = document.getElementsByClassName('MapUser_MapUser_160Xx')[1].id;
-    //     var gtrans = document.getElementById(get_g_id);
-    //     var attrvalue = "translate("+valueX+","+valueY+")";
-    //     gtrans.setAttribute("transform",attrvalue);
-    //     ++v;
-    // }
-    
-    // var rect_animate = document.getElementById("rect_animate");
-    // var rect_animate_y = document.getElementById("rect_animate_y");
-    // var text_animate_x = document.getElementById("text_animate_x");
-    // var text_animate_y = document.getElementById("text_animate_y");
-
-    //             rect_animate.setAttribute("attributeName","x");
-    //             rect_animate.setAttribute("dur","3s");
-    //             rect_animate.setAttribute("to",valueX);
-    //             rect_animate.setAttribute("from", getx);
-    //             rect_animate.setAttribute("repeatCount", "linear");
-                
-    //             text_animate_x.setAttribute("attributeName","x");
-    //             text_animate_x.setAttribute("dur","3s");
-    //             text_animate_x.setAttribute("to",valueX);
-    //             text_animate_x.setAttribute("from", getx);
-    //             text_animate_x.setAttribute("repeatCount", "linear");
-
-    //             text_animate_y.setAttribute("dur", "3s");
-    //             text_animate_y.setAttribute("attributeName","y");
-    //             text_animate_y.setAttribute("to", valueY);
-    //             text_animate_y.setAttribute("from", gety);
-    //             text_animate_y.setAttribute("repeatCount", "linear");
-
-    //             rect_animate_y.setAttribute("dur", "3s");
-    //             rect_animate_y.setAttribute("attributeName","y");
-    //             rect_animate_y.setAttribute("to", valueY);
-    //             rect_animate_y.setAttribute("from", gety);
-    //             rect_animate_y.setAttribute("repeatCount", "linear");
-    
-                // rect.setAttribute("x", valueX);
-                // rect.setAttribute("y", valueY);
-                // text.setAttribute("x", valueX);
-                // text.setAttribute("y", valueY);
+      
 }
 function changeDimensions(click , message) {
-   var v1= count[0];
-   var v2=count[1];
-   var v3=count[2];
-   var v4=count[3];
-   var v5=count[4];
    var i=0;
     const get_id = count;
     for(i=0; i<count.length; i++){
@@ -284,135 +96,9 @@ function changeDimensions(click , message) {
             socket.emit('value_of_x', x,y);
             socket.emit('value_of_y', y); 
             socket.emit('username',one);
-            console.log("moving user_ "+one);
         }
     }
-    // if(v1==one){
-      
-    //             // console.log('message: '+message);
-    // }
-    // if(v2==one){
-    //     var get_g_id = document.getElementsByClassName('MapUser_MapUser_160Xx')[1].id;
-    //             var gtrans = document.getElementById(get_g_id);
-    //             var x = click.clientX;
-    //             var y = click.clientY;
-    //             var attrvalue = "translate("+x+","+y+")";
-    //             gtrans.setAttribute("transform",attrvalue);
-    //             socket.emit('value_of_x', x,y);
-    //             socket.emit('value_of_y', y); 
-    //             socket.emit('username',one);
-    //             console.log("moving user_ "+one)
-    //             i++;
-    //             console.log('value of i='+i);
-    // }
-    // if(v3==one){
-    //     var get_g_id = document.getElementsByClassName('MapUser_MapUser_160Xx')[2].id;
-    //     var gtrans = document.getElementById(get_g_id); 
-    //     var x = click.clientX;
-    //     var y = click.clientY;
-    //     var attrvalue = "translate("+x+","+y+")";
-    //     gtrans.setAttribute("transform",attrvalue);
-    //     socket.emit('value_of_x', x,y);
-    //     socket.emit('value_of_y', y); 
-    //     socket.emit('username',one);
-    //     console.log("moving user_ "+one)
-    //     i++;
-    //     console.log('value of i='+i);
-    // }
-    // if(v4==one){
-    //     var get_g_id = document.getElementsByClassName('MapUser_MapUser_160Xx')[3].id;
-    //     var gtrans = document.getElementById(get_g_id); 
-    //     var x = click.clientX;
-    //     var y = click.clientY;
-    //     var attrvalue = "translate("+x+","+y+")";
-    //     gtrans.setAttribute("transform",attrvalue);
-    //     socket.emit('value_of_x', x,y);
-    //     socket.emit('value_of_y', y); 
-    //     socket.emit('username',one);
-    //     console.log("moving user_ "+one)
-    //     i++;
-    //     console.log('value of i='+i);
-    // }
-    // if(v5==one){
-    //     var get_g_id = document.getElementsByClassName('MapUser_MapUser_160Xx')[4].id;
-    //     var gtrans = document.getElementById(get_g_id); 
-    //     var x = click.clientX;
-    //     var y = click.clientY;
-    //     var attrvalue = "translate("+x+","+y+")";
-    //     gtrans.setAttribute("transform",attrvalue);
-    //     socket.emit('value_of_x', x,y);
-    //     socket.emit('value_of_y', y); 
-    //     socket.emit('username',one);
-    //     console.log("moving user_ "+one)
-    //     i++;
-    //     console.log('value of i='+i);
-    // }     
-                
-            
-            
-        // if(v1==one)
-        // {var get_g_id = document.getElementsByClassName('MapUser_MapUser_160Xx')[0].id;
-        // var gtrans = document.getElementById(get_g_id);
-        // var x = click.clientX;
-        // var y = click.clientY;
-        // var attrvalue = "translate("+x+","+y+")";
-        // gtrans.setAttribute("transform",attrvalue);
-        // socket.emit('value_of_x', x,y);
-        // socket.emit('value_of_y', y); 
-        // console.log("moving first user");}
-            
-        // else if(v2==one){
-        //     var get_g_id = document.getElementsByClassName('MapUser_MapUser_160Xx')[1].id;
-        //     var gtrans = document.getElementById(get_g_id);
-        //     var x = click.clientX;
-        //     var y = click.clientY;
-        //     var attrvalue = "translate("+x+","+y+")";
-        //     gtrans.setAttribute("transform",attrvalue);
-        //     socket.emit('value_of_x', x,y);
-        //     socket.emit('value_of_y', y); 
-        //     console.log("moving second user");
-        // }
-        // else if(v3==one){
-        //     var get_g_id = document.getElementsByClassName('MapUser_MapUser_160Xx')[2].id;
-        //     var gtrans = document.getElementById(get_g_id);
-        //     var x = click.clientX;
-        //     var y = click.clientY;
-        //     var attrvalue = "translate("+x+","+y+")";
-        //     gtrans.setAttribute("transform",attrvalue);
-        //     socket.emit('value_of_x', x,y);
-        //     socket.emit('value_of_y', y); 
-        //     console.log("moving third user");
-        // }
-    // if(count[0]){
-       
-    // }
-    // else if(count[1]){
-        
-    // }
-    // else{
-    //     console.log('Count not Found!!!');
-    // }
-        
-
-    
-    // var  rect_get_id = document.getElementById(v);
-    // var text_get_id = document.getElementById(v);
-    // // var rect2 = document.getElementById
-    // var text= document.getElementById(get_id+"text");
-    // var rect = document.getElementById(get_id);
-    // var getx= rect.getAttribute("x");
-    // var gety = rect.getAttribute("y");
-    // var x = click.clientX;
-    // var y = click.clientY;
-    // var attrvalue = "translate("+x+","+y+")";
-    
-    
-    // g.setAttribute("y", y);
-    // text.setAttribute("x", x)
-    // text.setAttribute("y",y);
- 
-    
-
+   
 }
 function getDistance(x1, y1, x2, y2){
     let xDistance = x2-x1;
@@ -425,10 +111,6 @@ function getMousePosition(click){
     let rects = mainDiv.getBoundingClientRect();
     var x = click.clientX - rects.offsetWidth/2 ;
     var y = click.clientY - rects.offsetheight/2;
-    // console.log(rects);
-    // console.log(x);
-    // console.log(y);
-    // console.log(click.top);
 }
 ctx.addEventListener('mousedown' , (e) =>{
     getMousePosition(ctx, e)
--- server.js
+++ server.js
@@ -23,15 +23,12 @@ function connected (socket){
     })
     socket.on('value_of_x', x =>{
         socket.broadcast.emit('updated_x', x);
-        console.log(x);
     })
     socket.on('value_of_y', y =>{
         socket.broadcast.emit('updated_y', y);
-        console.log(y);
     })
     socket.on('username', username =>{
         socket.broadcast.emit('update_user', username);
-        console.log(username);
     })
     socket.on("send_user_class" ,data=>{
         
@@ -41,26 +38,3 @@ function connected (socket){
         delete users[socket.id]
     })
 }
-
-// io.on('connection', connected);
-
-//listening to events after the connection is estalished
-// function connected(socket){
-//     socket.on('newUser', data => {
-//         console.log("New client connected, with id: "+socket.id);
-//         users[socket.id] = data;
-//         console.log("Starting position: "+users[socket.id].x+" - "+users[socket.id].y);
-//         console.log("Current number of users: "+Object.keys(users).length);
-//         console.log("players dictionary: ", users);
-//         io.emit('updatePlayers', users);
-//     })
-//     socket.on('disconnect', function(){
-//         delete users[socket.id];
-//         console.log("Goodbye client with id "+socket.id);
-//         console.log("Current number of users: "+Object.keys(users).length);
-//         io.emit('updatePlayers', users);
-//     })
-//     socket.on('ClientClientHello', data => {
-//         socket.broadcast.emit('ServerClientHello', data);
-//     })
-// }
