0941b756c0167b20dd5d4b7934dd7d3d47e6d94c
Nouman Qureshi <noumanqureshi15@gmail.com>
2022-02-17 19:03:37 +0500

    Add files via upload

--- public/client.js
+++ public/client.js
@@ -1,22 +1,71 @@
 const socket =io.connect();
+const form = document.getElementById('send-container');
 const messagecontainer = document.getElementById('message-container');
 const messageForm = document.getElementById('send-button');
 const messageInput = document.getElementById('message-input')
+const roomInput = document.getElementById('message-input');
 var mainDiv = document.getElementById("main");
 var count =[];
 var limit = 5;
 var one;
-socket.emit('newUser', );
+var two;
+var id=[];
+var history2 = [];
+
+
+let clientRoom;
+let userId;
+console.log('home All Ids: '+id);
+socket.on('rooms', data=>{
+    console.log('connected Users: '+data.userno);
+    console.log('Room No: '+data.roomNo);
+    console.log('userId: '+data.userid);
+    
+})
+// socket.on('users',history =>{
+//     console.log('users from server: '+history);
+//     // history2.push(history);
+    
+// })
+
+socket.on('users',function(data2){
+    history2 = data2;
+    
+        for(var j =0; j<history2.length; j++){
+            if(history2[j]!=one){
+              
+                appendMessage(history2[j]);
+            }
+            
+        }    
+})
+ 
+socket.on('chat-message',  function(data){
+    appendMessage(data);
+   })
 
     const ctx = document.getElementById('map');
-socket.on('chat-message', data =>{
-    appendMessage(data)
+// socket.on('chat-message', data =>{
+//     for(var i = 0; i<history.length; i++){
+//         appendMessage(data[i]);
+//     }
+    
+// })
+socket.on('send-userid',usersid =>{
+    two=usersid;
 })
+socket.on('userDeleted',message =>{
+    
+        userdisconnected(message);
+    
+})
+
 // send your position..
 socket.on('updated_x' , value1=>{
     socket.on('updated_y' , value2=>{
         socket.on('update_user',username=>{
             var i;
+            usersFound = {}
             for(i=0; i<count.length; i++){
                 if(count[i] == username){
                     var get_g_id = document.getElementsByClassName('MapUser_MapUser_160Xx')[i].id;
@@ -30,31 +79,44 @@ socket.on('updated_x' , value1=>{
     })
     
  })
-
-socket.on('user-disconnect', data =>{
-    // console.log(data);
-    // var get_g_id = document.querySelector('g')[0];
-    // get_g_id.remove();
-})
 messageForm.addEventListener('click', e=>{
     e.preventDefault()
-    const message = messageInput.value
+    const message = messageInput.value;
     one=message;
-    socket.emit('send-chat-message', message)
+    socket.emit('send-chat-message', message );
     messageInput.value = ''
+    mainDiv.style.display="block";
+    form.style.display="none";
 })
+function userdisconnected(userid){
+
+    
+    var i=1;
+    const get_id = count;
+    usersFound ={};
+    for(i=1; i<id.length; i++){
+        if(id[i]== userid){
+            var get_g_id = document.getElementsByClassName('MapUser_MapUser_160Xx')[i].id;
+            var gtrans = document.getElementById(get_g_id);
+            gtrans.remove();
+            id.pop(userid);
+           console.log("disconnected this User: "+userid)
+        }
+        
+    }
+}
 //
 function appendMessage(message){
     
     const gettext = message;
     count.push(message);
-    
-    // console.log('Counter '+count);
+    id.push(two);
     var g_tag = document.createElementNS("http://www.w3.org/2000/svg","g");
             g_tag.setAttribute("id",gettext+"_user_1");
             g_tag.setAttribute("transform",`translate(0,0)`);
             g_tag.setAttribute("data-cy","map_user_me");
             g_tag.setAttribute("class","MapUser_MapUser_160Xx");
+            g_tag.setAttribute('onClick','Choose();');
             g_tag.style.transition="all 2s";
             document.getElementById("map").appendChild(g_tag);
             var createrect = document.createElementNS("http://www.w3.org/2000/svg","rect");
@@ -85,20 +147,37 @@ function transition(valueX, valueY){
 function changeDimensions(click , message) {
    var i=0;
     const get_id = count;
+    usersFound ={};
     for(i=0; i<count.length; i++){
         if(count[i]==one){
             var get_g_id = document.getElementsByClassName('MapUser_MapUser_160Xx')[i].id;
             var gtrans = document.getElementById(get_g_id);
+            get_value = gtrans.getAttribute('transform');
             var x = click.clientX;
             var y = click.clientY;
+            getx1 = x;
+            gety1=y;
             var attrvalue = "translate("+x+","+y+")";
             gtrans.setAttribute("transform",attrvalue);
             socket.emit('value_of_x', x,y);
             socket.emit('value_of_y', y); 
             socket.emit('username',one);
+            gety2 =y;
+            getx2=x;
+           
+            
         }
+        usersFound[i] =true;
     }
    
+   
+}
+function Choose() {
+    
+  }
+
+function videocalling(){
+  
 }
 function getDistance(x1, y1, x2, y2){
     let xDistance = x2-x1;
--- server.js
+++ server.js
@@ -10,17 +10,30 @@ server.listen(PORT, function(){
     console.log(`listing on ${PORT}`);
 })
 
+let history =[];
 let users = {};
+let userno = 0;
+let roomNo;
+let userid;
 io.on('connection', connected);
 function connected (socket){
-  
-    socket.on('send-chat-message', message =>{
-        console.log("new user connected with id: "+socket.id);
-        users[socket.id] == message;
-        console.log('total users: '+Object.keys(users).length);
-        socket.broadcast.emit('chat-message', message);
-        socket.emit('chat-message',message);
-    })
+            userno+1;
+            roomNo =1;
+            userid=socket.id;
+            socket.join(roomNo);
+            socket.emit('rooms', {userno: userno, roomNo: roomNo , userid:userid});
+            socket.on('send-chat-message', (message) =>{
+            console.log("new user connected with id: "+socket.id);
+            users[socket.id] = message;
+            history.push(message);
+            console.log('user name: '+message)
+            socket.emit('users', history);
+            console.log('total users: '+Object.keys(users).length);
+            socket.broadcast.emit('send-userid', socket.id);
+            socket.broadcast.emit('chat-message', message);
+            socket.emit('chat-message',message);
+        })
+    
     socket.on('value_of_x', x =>{
         socket.broadcast.emit('updated_x', x);
     })
@@ -33,8 +46,12 @@ function connected (socket){
     socket.on("send_user_class" ,data=>{
         
     })
-    socket.on('disconnect', () =>{
-        socket.broadcast.emit('user-disconnect', users[socket.id])
-        delete users[socket.id]
+    socket.on('disconnect', message =>{
+        delete users[socket.id];
+        console.log("Goodbye client with id :"+socket.id);
+        console.log("Current number of Users: "+Object.keys(users).length);
+        socket.broadcast.emit('userDeleted',socket.id);
+
     })
 }
+
